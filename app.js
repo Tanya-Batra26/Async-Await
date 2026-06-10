ff29b001f5e5c6af9720629a50508885fa2fe270
Tanya-Batra26 <tanya.batra@techcarrot.ae>
2026-06-10 12:00:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+async function fetchUsers() {
+
+    const loading = document.getElementById("loading");
+    const usersTable = document.getElementById("users");
+
+    try {
+
+        loading.textContent = "Loading users...";
+        usersTable.innerHTML = "";
+
+        const response = await fetch(
+            "https://jsonplaceholder.typicode.com/users"
+        );
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch users");
+        }
+
+        const users = await response.json();
+
+        users.forEach(user => {
+
+            usersTable.innerHTML += `
+                <tr>
+                    <td>${user.name}</td>
+                    <td>${user.email}</td>
+                </tr>
+            `;
+        });
+
+    } catch (error) {
+
+        usersTable.innerHTML = `
+            <tr>
+                <td colspan="2">
+                    Error: ${error.message}
+                </td>
+            </tr>
+        `;
+
+    } finally {
+
+        loading.textContent = "";
+    }
+}
---
+++ then.js
@@ -0,0 +1,22 @@
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => {
+        console.log(users);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+    async function fetchUsers() {
+    try {
+        const response = await fetch(
+            "https://jsonplaceholder.typicode.com/users"
+        );
+
+        const users = await response.json();
+
+        console.log(users);
+    } catch (error) {
+        console.log(error);
+    }
+}
